0389b1ea8a2e2f0cdda928fa0941daf54587432b
Evgeniy Baranov <master@kompolom.ru>
2016-07-12 22:45:07 +0300

    автогенерация иконок

---
+++ generate-icons.js
@@ -0,0 +1,64 @@
+'strict mode';
+
+const fs = require('fs');
+const path = require('path');
+const ICONS_ROOT = path.resolve('libs/material-design-icons');
+const ICONS_PATH = 'svg/production';
+const BASE_SIZE = 24;
+
+var mods = [];
+var icons = [];
+var sources = [];
+
+var scopes = ['action', 'alert', 'av', 'communication', 'content', 'device', 'editor', 'file', 'hardware'];
+sources.push(scopes.map(processScope));
+
+function processScope(scopeName){
+    var scopeDir = path.resolve(ICONS_ROOT, scopeName, ICONS_PATH);
+    var scopeIcons = fs.readdirSync(scopeDir);
+    var filteredIcons = scopeIcons.filter(filterIcon);
+    var bemEntites = filteredIcons.map(bemify);
+
+    bemEntites.map(function(entity){
+        var filePath = 'common.blocks/icon/_'+entity.modName,
+            fileModVal = entity.modVal !== true? '_' + entity.modVal : '',
+            templateName = 'icon_' + entity.modName + fileModVal + '.bemhtml.js';
+        try{fs.accessSync(filePath)}
+        catch (e) {
+            if(e.code == 'ENOENT')
+                fs.mkdirSync(filePath);
+        }
+        return fs.writeFileSync(path.resolve(filePath, templateName), getTemplateContent(
+            entity.modName,
+            entity.modVal,
+            fs.readFileSync(path.resolve(scopeDir, entity.file), 'utf8')
+        ));
+    });
+    return bemEntites;
+}
+
+function filterIcon(filename){
+    var fileData = filename.split('_');
+    return fileData.pop() === BASE_SIZE+'px.svg';
+}
+
+function bemify(filename){
+    var fileData = filename.split('_');
+    var entity = {
+        file : filename,
+        modName : fileData[1],
+        modVal : fileData.length === 4? fileData[2] : true
+    };
+    return entity;
+}
+
+function getTemplateContent(mod, val, svg){
+    var modVal = val === true || '\''+val+'\'';
+    return [
+        'block(\'icon\').mod(\'material\', true).mod(\''+mod+'\', '+modVal+')(',
+        '    content()(\''+svg+'\')',
+        ');'
+    ].join('\n');
+}
+
+module.exports = icons;
